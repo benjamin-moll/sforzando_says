4b2a418ba8b136b2e87d42aa1daf34aa0e05e4b8
benjamin-moll <54908849+benjamin-moll@users.noreply.github.com>
2020-05-06 20:43:58 -0400

    Added sufficient comments. Likely, final commit

--- sf_says_final/sf_says_final.ino
+++ sf_says_final/sf_says_final.ino
@@ -1,11 +1,12 @@
-//free play: all 4 notes play when held down
+//Free Play: all 4 notes play when held down
 //Beginner Difficulty: 2 notes, continuous sequence
 //Medium Difficulty: 3 notes, continuous sequence
 //Expert Difficulty: 4 notes, continuous sequence
-//play along: song played through speaker, free play mode available to play along
+//Master: Special easter egg pattern played and playable back
 
 #include <MIDIUSB.h>
 
+//define the MIDI notes and frequencies used
 const int NOTE_C3 = 60;
 const int FREQ_C3 = 131;
 
@@ -18,50 +19,59 @@ const int FREQ_C4 = 261;
 const int NOTE_E4 = 76;
 const int FREQ_E4 = 329;
 
+//define the pin input buttons
+
 const int C3_Button = 2;
 const int G3_Button = 3;
 const int C4_Button = 5;
 const int E4_Button = 6;
 const int sel_button = 9;
 
-
+//define the default state to compare against for button presses
 int lastC3State = HIGH;
 int lastG3State = HIGH;
 int lastC4State = HIGH;
 int lastE4State = HIGH;
 int lastSelState = HIGH;
 
-
+//define a few boolean variables as toggles for certain modes of the program
 bool played = false;
 bool generate = false;
 bool selected = false;
 
+//define a few counters for ensuring things only play once
 int counter = 0;
 int play_counter = 0;
 int win_counter = 0;
 int p_count = 0;
 
+//define the default mode (free play) and another boolean check variable
 int mode = 1;
 bool start = false;
 
-
+//define the start, current and max level variables
 int initial_level = 2;
 int current_level = 2;
 const int max_level = 10;
 
-//sequence of notes
+//sequence of notes avaiable when sequence will be generated
 int notes[] = {NOTE_C3, NOTE_G3, NOTE_C4, NOTE_E4};
 
+//define a variable to be used to select the number of notes avaiable to select for each difficulty, default 4
 int noteSize = 4;
 
+//create 2 arrays, one to store the computer generated note list and one to store the user input sequence
 int sf_sequence[max_level];
 int in_sequence[max_level];
 
+//define timers to use instead of delays in some portions of the program
 unsigned long newTimer1 = 0;
 unsigned long newTimer2 = 0;
 
 
 void setup() {
+  //map the buttons as input_pullup to avoid needing resistors
+  
   pinMode(C3_Button, INPUT_PULLUP);
   pinMode(G3_Button, INPUT_PULLUP);
   pinMode(C4_Button, INPUT_PULLUP);
@@ -75,7 +85,7 @@ void setup() {
 }
 
 void loop() {
-  //makes program wait 2 seconds before printing instruction line, otherwise won't display in SM
+  //makes program wait 2 seconds before printing instruction line, otherwise won't display in Serial Monitor
   unsigned long currentTime = millis();
   if ((currentTime - newTimer2 >= 2000)) {
     newTimer2 = currentTime;
@@ -105,7 +115,7 @@ void loop() {
     }
   }
 
-
+  //check to see if we have reached the max level and if so user wins
   else if (current_level == max_level) {
     if (win_counter == 0) {
       delay(1000);
@@ -124,16 +134,16 @@ void loop() {
 int sel_state() {
   //read our select button
   int state = digitalRead(sel_button);
-
+  //if there is a change in state (i.e. it has been pressed)
   if (lastSelState != state) {
-
+  //and if that state is low
     if (state == LOW) {
-      //if pressed, increment mode counter
+      //if select pressed, increment mode counter
       delay(20);
       mode++;
 
 
-      //don't let mode go higher than 4 and rollover to 1 if button pressed again
+      //don't let mode go higher than 5 and rollover to 1 if button pressed again
       mode = mode % 6;
       if (mode < 1 || mode == 6) mode = 1;
 
@@ -145,7 +155,9 @@ int sel_state() {
     lastSelState = state;
   }
 
-
+  //after 7 seconds to allow user time to select, print out the mode they have chosen and then toggle the select mode boolean to true 
+  //once true we begin gameplay of the corresponding mode selected
+  
   unsigned long currentTime1 = millis();
   if ((currentTime1 - newTimer1 >= 7000) && counter == 1) {
     newTimer1 = currentTime1;
@@ -160,7 +172,7 @@ int sel_state() {
 }
 
 
-
+//this function prints out the mode selected and then calls the appropriate function based on that selection
 int game_mode(int mymode) {
 
   if (mymode == 1) {
@@ -216,6 +228,9 @@ int game_mode(int mymode) {
 
 }
 
+//this function calls the generate sequence function once and then toggles itself off then calls the MIDI player function 
+//to play out the sequence of notes from index 0 up to index current level of the sf_sequence array
+//finally it then runs the get user sequence function and waits for the result of that function
 void run_game() {
   if ((!generate) && current_level < max_level) {
     delay(500);
@@ -236,6 +251,9 @@ void run_game() {
 
 }
 
+//this function runs only once at the very beginning of Beginner, Intermediate and Expert Difficulty
+//it generates a randomly seeded sequence of notes and stores it into our pre-made sf_sequence array
+//for playing and comparing against when users are inputting a sequence
 int generate_sfsequence(int mySize) {
 
   //makes sequence ACTUALLY random
@@ -252,6 +270,8 @@ int generate_sfsequence(int mySize) {
 
 }
 
+//this function is a standard MIDI player function which checks for a button press of any of the note buttons
+//and simply plays that corresponding note through Sforzando Says as long as it is held in the LOW state
 void freePlay() {
 
   if (p_count == 0) {
@@ -276,8 +296,6 @@ void freePlay() {
     {
       //play note C3
       midiCmd(0x90, NOTE_C3, 0x60);
-      //delay(1000);
-      //      midiCmd(0x80, NOTE_C3, 0x00);
 
     }
     else if (C3_State == HIGH) {
@@ -291,8 +309,7 @@ void freePlay() {
     {
       //play note G3
       midiCmd(0x90, NOTE_G3, 0x60);
-      //      delay(1000);
-      //      midiCmd(0x80, NOTE_G3, 0x00);
+
     }
     else if (G3_State == HIGH) {
       delay(10);
@@ -304,8 +321,7 @@ void freePlay() {
     {
       //play note C3
       midiCmd(0x90, NOTE_C4, 0x60);
-      //      delay(1000);
-      //      midiCmd(0x80, NOTE_C4, 0x00);
+
     }
     else if (C4_State == HIGH) {
       delay(10);
@@ -330,9 +346,10 @@ void freePlay() {
   lastC4State = C4_State;
   lastE4State = E4_State;
 
-
-
 }
+
+//this function plays out the iconic 2001: A Space Odyssey theme song as the opening of Master difficulty
+//through Sforzando
 
 void odyssey_melody() {
   midiCmd(0x90, NOTE_C3, 0x60);
@@ -370,6 +387,8 @@ void odyssey_melody() {
 
 }
 
+//this function prints out instructions on what notes to play to mimic the theme song and then jumps to free play
+//to let the user try it out themselves
 void play_odyssey() {
 
   if (play_counter == 0) {
@@ -392,11 +411,15 @@ void play_odyssey() {
 
 
 
-
+//this is the default MIDI player function for Beginner, Intermediate and Expert difficulties
+//It play an initial sequence of notes denoted in the sf_sequence list
+//For beginner and intermediate, it plays each note in the sequence for one second
+//For expert, it plays each note in the sequence for half a second for an added challenge
+//To play a MIDI note we call a midiCMD() function defined below on channel 0x90, playing the sequence[i]
+//then we send a 0x80 message to turn that note off and then increment the index of sf_sequence up to the current_level'th index
 void sfSays() {
 
 
-  //play an initial sequence of notes denoted in the sf_sequence list
   if (mode == 2 or mode == 3) {
     for (int i = 0; i < current_level; i++) {
       midiCmd(0x90, sf_sequence[i], 0x60);
@@ -416,8 +439,11 @@ void sfSays() {
 
 }
 
+//this function is called to listen and store user inputs into the in_sequence array
+//It then checks that added index to the exact corresponding index of sf_sequence to ensure the user has matched the melody
+//if all notes are correctly entered we call the levelUp() function
 void get_sequence() {
-
+  //initialize a variable to tell the function to continue running, only set to break out if and when the user gets the entire sequence right
   int correct = 0;
   for (int i = 0; i < current_level; i++) {
     correct = 0;
@@ -555,6 +581,8 @@ void get_sequence() {
   levelUp();
 }
 
+//this function is called when the user gets the sequence played correct and incremements the level
+//it also sets toggle boolean variables for the MIDI player function back to their default states
 bool levelUp() {
 
   if (current_level < max_level) {
@@ -576,6 +604,7 @@ bool levelUp() {
   return selected;
 }
 
+//this function prints out the losing message and resets most parameters to their default
 bool gameOver() {
 
   Serial.println("You Lose!");
@@ -596,8 +625,6 @@ bool gameOver() {
 
 
 }
-
-
 
 //midi command generic function
 void midiCmd(byte cmd, byte data1, byte data2) {
